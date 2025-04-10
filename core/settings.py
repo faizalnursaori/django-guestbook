79604979b01f8d09e143ffad8779bccca468a494
Faizal Nursaori <faizalrahmatn@gmail.com>
2025-04-10 19:28:36 +0700

    fix default port

--- core/settings.py
+++ core/settings.py
@@ -141,7 +141,7 @@ STORAGES = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
